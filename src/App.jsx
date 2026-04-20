7dc7c4068bf50d76d7ecd1c469df5253dfec0f18
Joshua Ihesiulo <joshuaihesiulo@gmai.com>
2026-04-20 15:16:19 +0100

    Added the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Header from "./components/Header"
+import ProfileCard from "./components/ProfileCard"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-  </>
+    <div>
+    <Header/>
+    <div className="px-10">
+    <ProfileCard/>
+    </div>
+  </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+
+    return (
+        <header className="bg-blue-950 text-white p-4 text-center">
+            <h1 className="text-3xl font-bold ">RAD5 Tech Hub</h1>
+            <p className="text-xl">Training the Next Generation Of Developers</p>
+        </header>
+    )
+}
+export default Header
